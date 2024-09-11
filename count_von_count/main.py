3d19390bc17c428821a65f7377746188ac294e34
nolandseigler <57370691+nolandseigler@users.noreply.github.com>
2024-09-10 21:31:08 -0400

    decent progress and structure. anymore processes than one and this thing grinds to a halt. prob not large enough data to warrant more procs

--- count_von_count/main.py
+++ count_von_count/main.py
@@ -1,4 +1,9 @@
+
+import re
+import time
+from typing import Union
 from pypdf import PdfReader
+import spacy.tokens
 
 """
 Process each page and just save the highest number.
@@ -10,67 +15,217 @@ The reality is that we know the document structure and there is likely some summ
 that we should optimize for. this would prevent this program from being generalized so be careful.
 
 """
-
+from enum import StrEnum
 # python -m spacy download en_core_web_sm
 import spacy
 
-def main():
-    nlp = spacy.load("en_core_web_sm")
-    # Merge noun phrases and entities for easier analysis
-    nlp.add_pipe("merge_entities")
-    nlp.add_pipe("merge_noun_chunks")
+class Multipliers(StrEnum):
+    BILLION = "billion"
+    MILLION = "million"
+    THOUSAND = "thousand"
 
-    reader = PdfReader("/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf")
-    for page in reader.pages:
-        # TODO: better with or without newlines?
-        # def slower but might make processing easier?
-        text = page.extract_text().replace("$ ", "$")
-        # .replace("\n", "").replace("  ", " ")
-        # TODO: another gross O(n^2) thing that is prob not great
-        in_millions_page = "(Dollars in Millions)" in text and "Budget Estimates" in text
-        in_thousands_page = "(Dollars in Thousands)" in text and "Budget Estimates" in text
-        in_millions_summary_page = "AFWCF Financial Summary" in text and "(Dollars in Millions)" in text
-        # TODO: prob something that can be done to optimize this
-        # https://spacy.io/usage/linguistic-features#section-dependency-parse:~:text=The%20dependency%20parse,9.4%20million%22.
-        if "billion" in text:
-            pass
-        for doc in nlp.pipe(texts=[text]):
+
+class Counter:
+
+    BILLION = 1_000_000_000
+    MILLION = 1_000_000
+    THOUSAND = 1_000
+
+    _nlp: spacy.language.Language
+    _reader: PdfReader
+    _num_processing_proc: int
+    _garbage_filter: re.Pattern
+    _page_batch_size: int
+
+    _largest_num_token_lemma: str
+    _largest_num: float
+
+    _no_ctx_largest_num_token_lemma: str
+    _no_ctx_largest_num: float
+
+    __slots__ = (
+       "_nlp",
+       "_reader",
+       "_num_processing_proc",
+       "_garbage_filter",
+       "_page_batch_size",
+       "_largest_num_token_lemma",
+       "_largest_num",
+       "_no_ctx_largest_num_token_lemma",
+       "_no_ctx_largest_num",
+    )
+
+    def __init__(self, pdf_filepath: str, page_batch_size: int = 10, num_processing_proc: int = 1) -> None:
+        nlp = spacy.load("en_core_web_sm")
+        # # Merge noun phrases and entities for easier analysis
+        nlp.add_pipe("merge_entities")
+        self._nlp = nlp
+        self._reader = PdfReader(pdf_filepath)
+        self._garbage_filter = re.compile(r"\b(?:\d{1,3}(?:,\d{3})*)\$")
+        self._page_batch_size = page_batch_size
+
+        self._num_processing_proc = num_processing_proc
+
+        self._largest_num_token_lemma = ""
+        self._largest_num = 0.0
+
+        self._no_ctx_largest_num_token_lemma = ""
+        self._no_ctx_largest_num = 0.0
+
+    def _count_split_no_ctx(self, split: list[str], lemma: str):
+        for x in split:
+            x = x.replace("$", "").replace(",", "")
+            if not x.isnumeric() and "." not in x:
+                continue
+            try:
+                x = float(x)
+            except ValueError:
+                continue
+            if x > self._no_ctx_largest_num:
+                self._no_ctx_largest_num = x
+                self._no_ctx_largest_num_token_lemma = lemma
+
+    def _counts_split_len_2(self, split: list[str], lemma: str):
+        # ['$1,088.6', 'million']
+        num = split[0].replace("$", "").replace(",", "")
+        if not num.isnumeric() and "." not in num:
+            return
+        try:
+            num = float(num)
+        except ValueError:
+            return
+
+        # ordered by most common first
+        if split[1] == Multipliers.MILLION:
+            val = num * self.MILLION
+        elif split[1] == Multipliers.THOUSAND:
+            val = num * self.THOUSAND
+        elif split[1] == Multipliers.BILLION:
+            val = num * self.BILLION
+        else:
+            return
+
+        if val > self._largest_num:
+            self._largest_num = val
+            self._largest_num_token_lemma = lemma
+
+    def _count_money_token(self, token: spacy.tokens.Token):
+        if self._garbage_filter.match(token.lemma_) is not None:
+            return
+
+        splitty = token.lemma_.lower().replace("\n", "").replace("\r", "").replace("\t", "").replace("$ ", "$").strip().split(" ")
+
+        self._count_split_no_ctx(split=splitty, lemma=token.lemma_)
+
+        splitty_len = len(splitty)
+        # just going to take the L on these for now
+        if splitty_len != 2:
+            return
+
+        self._counts_split_len_2(split=splitty, lemma=token.lemma_)
+
+    def _count_pages(self, pages: list[str]):
+        for doc in self._nlp.pipe(texts=pages, n_process=self._num_processing_proc):
             for token in doc:
-                if "billion" in token.lemma_:
-                    pass
-                # nlp.meta["labels"]["ner"]
-                # ['CARDINAL', 'DATE', 'EVENT', 'FAC', 'GPE', 'LANGUAGE', 'LAW', 'LOC', 'MONEY', 'NORP', 'ORDINAL', 'ORG', 'PERCENT', 'PERSON', 'PRODUCT', 'QUANTITY', 'TIME', 'WORK_OF_ART']
-
-                # any other way to get a list of all ent_type_ ??
-
-                # here is from smashing them all in this doc into a sec: {'', 'ORDINAL', 'EVENT', 'DATE', 'NORP', 'PERSON', 'GPE', 'WORK_OF_ART', 'PERCENT', 'LAW', 'QUANTITY', 'PRODUCT', 'MONEY', 'CARDINAL', 'FAC', 'ORG', 'TIME', 'LOC'}
                 if token.ent_type_ == "MONEY":
-                    splitty = token.lemma_.split(" ")
-                    # .replace("\n", "").replace("  ", " ")
-                    # ['1']
-                    if len(splitty) != 2:
-                        continue
-                    # ['$1,088.6', 'million']
-                    if splitty[1] == "million":
-                        continue
-                    if splitty[1] == "thousand":
-                        continue
-                    if splitty[1] == "billion":
-                        continue
-                    # some of these will have a length of 2 and maybe one
-                    # and they will be like page 16 where we can pull the table header
-                    # ($ Millions) FY 2023 FY 2024 FY 2025
-                    # to calculate or unit
-                    pass
+                    self._count_money_token(token=token)
+
                 # theres something to be had here but it might not be worth the effort at the moment.
                 # this seems to hit on the tables the most
                 # example outputs
                 # 0.000000 179.76 0.000
                 # .012 .742 .693
+                # TODO: take the L on these for now
                 elif token.ent_type_ == "QUANTITY":
                     pass
-        if "billion" in text:
-            pass
+
+    def process(self) -> dict[str, dict[str, Union[str, float]]]:
+        # https://spacy.io/usage/processing-pipelines
+        pages = []
+        for page in self._reader.pages:
+            pages.append(page.extract_text())
+            # TODO: prob something that can be done to optimize this
+            # https://spacy.io/usage/linguistic-features#section-dependency-parse:~:text=The%20dependency%20parse,9.4%20million%22.
+            if len(pages) == self._page_batch_size:
+                self._count_pages(pages=pages)
+                pages.clear()
+
+        if pages:
+            self._count_pages(pages=pages)
+
+        return {
+            "largest": {
+                "num": self._largest_num,
+                "lemma": self._largest_num_token_lemma,
+            },
+            "no_context_largest": {
+                "num": self._no_ctx_largest_num,
+                "lemma": self._no_ctx_largest_num_token_lemma,
+            }
+        }
+
+
+
+
+def main():
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=1
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=10
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=100
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=1,
+        num_processing_proc=2,
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=10,
+        num_processing_proc=2,
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+    counter = Counter(
+        pdf_filepath="/Users/nolandseigler/CodeProjects/count-von-count/data/fy25_air_force_working_capital_fund.pdf",
+        page_batch_size=100,
+        num_processing_proc=2,
+    )
+    start = time.time()
+    results = counter.process()
+    end = time.time()
+    print(f"executed in {end - start} seconds, {counter._page_batch_size=}, {counter._num_processing_proc=}  {results=}")
+
+
 
 
 
